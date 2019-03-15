1deaa4293a12d2d601c368f99b23d8bf82747390
soufiane19745 <45607146+soufiane19745@users.noreply.github.com>
2019-03-15 19:20:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2586,7 +2586,7 @@ client.on('message', message => {
 if(message.channel.type === "dm") return;
 if(message.author.bot) return;
   if(!sWlc[message.guild.id]) sWlc[message.guild.id] = {
-    channel: "welcome"
+    channel: "💖الترحيب💖"
 }
 const channel = sWlc[message.guild.id].channel
   if (message.content.startsWith(prefix + "setwelcomer")) {
